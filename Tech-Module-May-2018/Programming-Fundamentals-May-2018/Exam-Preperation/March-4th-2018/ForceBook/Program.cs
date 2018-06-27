aedc0c026a0f5527ae6be122ba43de621a64f56b
Simo Aleksandrov <simo3003@me.com>
2018-06-27 19:00:21 +0300

    Add part 2 of March exam

---
+++ Tech-Module-May-2018/Programming-Fundamentals-May-2018/Exam-Preperation/March-4th-2018/ForceBook/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            //         side  -> members 
+            Dictionary<string, List<string>> database = new Dictionary<string, List<string>>();
+            while (input != "Lumpawaroo")
+            {
+                if (input.Contains(" | ")) // can the names contain | ?
+                {
+                    string[] splitInput = input.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string forceSide = splitInput[0];
+                    string forceUser = splitInput[1];
+                    bool isUserInASide = false;
+                    foreach (var side in database.Values.ToList())
+                    {
+                        if (side.Contains(forceUser)) // check if the user is in any of the sides
+                        {
+                            isUserInASide = true;
+                        }                        
+                    }
+
+                    if (isUserInASide == false)
+                    {
+                        if (database.ContainsKey(forceSide))
+                        {
+                            database[forceSide].Add(forceUser);
+                        }
+                        else
+                        {
+                            database.Add(forceSide, new List<string>
+                            {
+                                forceUser    
+                            });
+                        }
+                    }
+                }
+                else if (input.Contains(" -> ")) // can the names contain -> ?
+                {
+                    string[] splitInput = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    string forceUser = splitInput[0];
+                    string forceSide = splitInput[1];
+                    bool isUserInASide = false;
+                    foreach (var side in database.Values.ToList())
+                    {
+                        if (side.Contains(forceUser)) // check if the user is in any of the sides
+                        {
+                            side.Remove(forceUser); // remove the user
+                            if (database.ContainsKey(forceSide)) // check if the given side exists
+                            {
+                                if (!database[forceSide].Contains(forceUser)) // check if the user is in the side they want
+                                {
+                                    database[forceSide].Add(forceUser); // add them
+                                }
+                            }
+                            else
+                            {
+                                database.Add(forceSide, new List<string>
+                                {
+                                    forceUser
+                                });
+                            }
+                            isUserInASide = true;
+                            break;
+                        }                        
+                    }
+
+                    if (isUserInASide == false)
+                    {
+                        if (database.ContainsKey(forceSide))
+                        {
+                            database[forceSide].Add(forceUser);
+                        }
+                        else
+                        {
+                            database.Add(forceSide, new List<string>
+                            {
+                                forceUser    
+                            });
+                        }
+                    }
+                    Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                }
+                input = Console.ReadLine();
+            }
+
+            foreach (var side in database.OrderByDescending(x=>x.Value.Count).ThenBy(x=>x.Key))
+            {
+                if (side.Value.Count>0)
+                {
+                    Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+                    foreach (var user in side.Value.OrderBy(x=>x))
+                    {
+                        Console.WriteLine($"! {user}");
+                    }
+                }
+            }
+        }
+    }
+}
