0bd4e93e2ea7c4c910acdca53fd576878ac11c01
Simo Aleksandrov <simo3003@me.com>
2018-08-06 20:38:52 +0300

    Add C# Calculator

---
+++ Tech-Module-May-2018/Software-Technologies-July-2018/Calculator-CSharp/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Calculator_CSharp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(Calculator calculator)
+        {
+            calculator.CalculateResult();
+
+            return RedirectToAction("Index", calculator);
+        }
+    }
+}
